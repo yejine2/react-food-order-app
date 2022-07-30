b40655ae1e793b6108b875ff201b68ae2a053a59
yejine2 <yejine2@naver.com>
2022-07-31 02:17:07 +0900

    feat: Add useRef

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,22 +1,43 @@
-import Input from '../../UI/Input'
-import classes from './MealItemForm.module.css'
+import { useRef } from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
-const MealItemForm = props => {
+const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    // 유효성 검사
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      return;
+    }
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        label='Amount' 
+        ref={amountInputRef}
+        label="Amount"
         input={{
-          id: 'amount' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1'
-      }}/>
+          id: "amount" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
-  )
+  );
 };
 
-export default MealItemForm
+export default MealItemForm;

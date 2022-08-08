49b5cfad1b6965e3198e0186b3be48e82e1e8057
yejine2 <yejine2@naver.com>
2022-08-08 21:49:12 +0900

    feat: Add submitOrderHandler function

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -25,6 +25,20 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  // 주문 요청 비동기 로직
+  const submitOrderHandler = (userData) => {
+    fetch(
+      "https://react-food-app-http-b0341-default-rtdb.firebaseio.com/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -60,7 +74,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
